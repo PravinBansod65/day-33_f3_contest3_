0051640b1ebd969799496c3859e20234d0006e98
silentSK987 <pravinbansod65@gmail.com>
2024-01-28 13:20:43 +0530

    doen

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', function() {
   
     // Fetching  data from API
     function fetchData() {
-      const apiUrl = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false';
+      const apiUrl = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false'
       return fetch(apiUrl)
         .then(response => response.json());
     }
